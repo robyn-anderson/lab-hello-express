5a0a470010ab4cc4a2ab34f73b8bff060e5c4ca4
robyn anderson <robynjanders@gmail.com>
2022-07-12 15:39:05 -0400

    adding hello express lab

---
+++ app.js
@@ -0,0 +1,54 @@
+const Express = require("express") // Call Express
+const { UNSAFE_NavigationContext } = require("react-router-dom")
+const app = Express() // Invoke Express
+
+const PORT = process.env.PORT || 5000
+/* 
+app.get('/', (req, res) => {
+    res.send('Hello, home!')
+})
+
+app.get('/tea-time', (rq, res) => {
+    res.send('<h5>I say,<h5><h4>don\'t you think</h4><h3>it\'s time</h3><h2>for a cup of</h2><h1>tea?</h1>')
+})
+
+app.get('/walkies', (rq, res) => {
+    res.send('<h1>Time for walkies.</h1><h2><em>Definitely</em> time for walkies.</h2>')
+})
+
+app.get('/cake', (req, res) => {
+    res.send('<h1>Is there <span style="color: magenta;">cake</span> here?</h1>')
+}) 
+*/
+
+app.get('/form', (req, res) => {
+    res.send(`
+    <form action="/welcome">
+    <input type="text" placeholder="your name" name="name">
+    <input type="submit">
+    </form>`) 
+    res.send(`Welcome ${res.query.welcome.name}`) //! this is not working!
+})
+
+
+
+app.get('/:page', (req, res) => {
+    //res.send(`You are on the ${req.params.page} page`)
+    if (req.params.page === 'cake') {
+        res.send('<h1>Is there <span style="color: magenta;">cake</span> here?</h1>')
+    }
+    if (req.params.page === 'walkies') {
+        res.send('<h1>Time for walkies.</h1><h2><em>Definitely</em> time for walkies.</h2>')
+    }
+    if (req.params.page === 'tea-time') {
+        res.send('<h5>I say,<h5><h4>don\'t you think</h4><h3>it\'s time</h3><h2>for a cup of</h2><h1>tea?</h1>')
+    }
+    else {
+        res.send(`You are on the ${req.params.page} page`)
+    }
+})
+ 
+
+app.listen(PORT, () =>{
+    console.log(`Listening on ${PORT}`)
+})
